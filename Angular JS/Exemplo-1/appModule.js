183ab94f3b8f353a1ffdaed0f28c7e60c85e30da
KaiqueJesus11 <39889431+KaiqueJesus11@users.noreply.github.com>
2022-08-31 20:33:07 -0300

    formularios no angular

--- Angular JS/Exemplo-1/appModule.js
+++ Angular JS/Exemplo-1/appModule.js
@@ -1,23 +1,45 @@
 angular.module('appModule', [])
     .controller('Principal', [
-        function() {
+        function () {
             var self = this
             self.alunos = [
                 {
                     nome: "Ademar Torres",
-                    curso: "Angular"
+                    curso: "Angular",
+                    done: false
                 },
                 {
                     nome: "Celilia Maria",
-                    curso: "MongoDB"
-                },{
+                    curso: "MongoDB",
+                    done: false
+                },
+                {
                     nome: "Adorian Barbosa",
-                    curso: "Node JS"
+                    curso: "Node JS",
+                    done: false
                 }
             ]
             self.mensagem = 'Vindo do controller'
+            
+            self.submit = () => {
+                self.alunos.push({nome: self.aluno.nome, curso: self.aluno.curso, done: false})
+                self.aluno.nome = ""
+                self.aluno.curso = ""
+            }
+
+            self.limpar = () => {
+                var alunosOld = self.alunos
+                self.alunos = []
+
+                alunosOld.forEach(aluno => {
+                    if(!aluno.done){
+                        self.alunos.push(aluno)
+                    }
+                })
+            }
+
             self.alterarMensagem = () => {
                 self.mensagem = "Mensagem alterada"
             }
-    }
-])
+        }
+    ])
